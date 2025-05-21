891a7fd546e8ad9cebbb1aa80ea22932d244cd8b
goseitutu1 <oseitutugideon1@gmail.com>
2025-05-21 09:49:21 +0000

    space

--- public/js/product.js
+++ public/js/product.js
@@ -18,6 +18,7 @@ const deleteProduct = btn => {
         productElement.parentNode.removeChild(productElement);
       })
       .catch(err => {
+        
         console.log(err);
       });
   };
